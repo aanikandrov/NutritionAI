0bbdea343ceb8db396e9e7d767a34d501560dbba
Андрей <a@>
2026-03-21 16:24:00 +0300

    llama -> mistral

--- app/config.py
+++ app/config.py
@@ -6,11 +6,14 @@ class Settings(BaseSettings):
     # Настройки приложения
     app_name: str = "Nutrition AI"
 
+    api_url: str = "http://127.0.0.1:8000/calculate"
+
     # Настройки Ollama
     ollama_url: str = "http://localhost:11434/v1"
     # Ollama не проверяет ключ
     ollama_api_key: str = "ollama_api_key"
-    model_name: str = "llama3.2"
+    # model_name: str = "llama3.2"
+    model_name: str = "mistral"
     max_tokens: int = 500
     default_temperature: float = 1.0
 
